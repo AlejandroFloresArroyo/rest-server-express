8af2dcdb8b4581c7687e96ca3346838b35730920
Alejandro Flores <itt.alejandro.flores@hotmail.com>
2020-04-23 07:59:46 -0500

    Agregada configuracion del env

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 // Puerto 
 
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno 
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// Base de datos
+
+let urlDB;
+
+//if (process.env.NODE_ENV === 'dev') {
+//    urlDB = 'mongodb://localhost:27017/cafe';
+//} else {
+    urlDB = 'mongodb+srv://ale:gatoloco2@cluster0-1vxog.mongodb.net/cafe';
+//}
+
+process.env.URLDB = urlDB;
+
+// mongodb: //mongodb://localhost:27017/cafe
+
+// mongodb: //mongodb+srv://ale:gatoloco2@cluster0-1vxog.mongodb.net/test
